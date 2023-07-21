c2ae0eafd4ca68bbc20368d42495ac08a3d18289
Eric Had Arach <hadarachericj@gmail.com>
2023-07-21 17:31:11 -0300

    Test css.

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+
+
+function handleSizeButtons(e) {
+  if (e.target === "button-sixteen") {
+    if (board.dataset.size === "16") {
+      return;
+    }
+    else {
+      board.dataset.size = "16";
+    }
+  }
+  else if (e.target === "button-thirty-two") {
+    if (board.dataset.size === "32") {
+      return;
+    }
+    else {
+      board.dataset.size = "32";
+    }
+  }
+  else {
+    if (board.dataset.size === "64") {
+      return;
+    }
+    else {
+      board.dataset.size = "64";
+    }
+  }
+  
+}
+
+function playGame() {
+  const boardContainer = document.querySelector("#board-container");
+  const board = document.querySelector("#board");
+
+}
