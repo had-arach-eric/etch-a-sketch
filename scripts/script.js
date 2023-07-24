c65ccee6aae18247b559f21c97cd752989480168
Eric Had Arach <hadarachericj@gmail.com>
2023-07-24 08:33:59 -0300

    Adding functionality to size buttons.

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,31 @@
-/*
 
-function handleSizeButtons(e) {
-  if (e.target === "button-sixteen") {
-    if (board.dataset.size === "16") {
-      return;
-    }
-    else {
-      board.dataset.size = "16";
-    }
-  }
-  else if (e.target === "button-thirty-two") {
-    if (board.dataset.size === "32") {
-      return;
-    }
-    else {
-      board.dataset.size = "32";
-    }
-  }
-  else {
-    if (board.dataset.size === "64") {
-      return;
-    }
-    else {
-      board.dataset.size = "64";
-    }
-  }
+
+
+
+function setNewBoard(boardUI, size) {
+  boardUI.dataset.size = size.toString();;
+  boardUI.innerHTML = null;
+  boardUI.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
   
+  for (let i = 0; i < size*size; i++) {
+    const cellUI = document.createElement("div");
+    boardUI.appendChild(cellUI);
+  }
 }
 
 function playGame() {
-  const boardContainer = document.querySelector("#board-container");
-  const board = document.querySelector("#board");
-
+  const boardUI = document.querySelector(".board-container");
+  const buttonsSizeUI = document.querySelectorAll(".container-buttons-size > button");
+  for (let i = 0; i < buttonsSizeUI.length; i++) {
+    buttonsSizeUI[i].addEventListener("click", e => {
+      const size = Number(e.target.dataset.size);
+      //console.log("se presiono");
+      setNewBoard(boardUI, size);
+    });
+  }
+  
+  
 }
 
-*/
+playGame();
+
