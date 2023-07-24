583d32c807e15facd09f15e91cb3f64f1ed3ec02
Eric Had Arach <hadarachericj@gmail.com>
2023-07-24 14:41:05 -0300

    Click on board to paint.

--- scripts/script.js
+++ scripts/script.js
@@ -2,39 +2,67 @@ function setNewBoard(boardUI, size) {
   boardUI.dataset.size = size.toString();;
   boardUI.innerHTML = null;
   boardUI.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
-  
-  for (let i = 0; i < size*size; i++) {
+
+  for (let i = 0; i < size * size; i++) {
     const cellUI = document.createElement("div");
     boardUI.appendChild(cellUI);
   }
 
 }
 
-
-
-function playGame() {
-
-/* PSEUDOCODIGO
-  Crear referencia a la cuadrícula
-  Crear cuadricula ante la presion de un botón de tamaño, si ya hay una cuadrícula se resetea
-  Establecer que cuando se mantenga presionado el click sobre el body Y se pase el mouse por algún item se active un listener que pinte de negro el item
-  Establecer que cuando se suelte el click se desactive el listener anterior.
-*/
-
-  const bodyUI = document.querySelector("body");
-  const boardUI = document.querySelector("#board-container");
-  const buttonsSizeUI = document.querySelectorAll(".button-size");
-
+function addListenersToSizeButtons(buttonsSizeUI, boardUI) {
   for (let i = 0; i < buttonsSizeUI.length; i++) {
     buttonsSizeUI[i].addEventListener("click", e => {
       setNewBoard(boardUI, Number(buttonsSizeUI[i].dataset.size));
     })
   }
-  
-  //setNewBoard(boardUI, 16);
+}
 
- 
+
+
+
+function playGame() {
+
+  let mode = "static";
+  let activeBrushButton; // almaceno el botón que va a estar activo
+  let clickPrint = { active: false };
   
+
+  //References to DOM nodes
+  const bodyUI = document.querySelector("body");
+  const boardUI = document.querySelector("#board-container");
+  const buttonsSizeUI = document.querySelectorAll(".button-size");
+
+  addListenersToSizeButtons(buttonsSizeUI, boardUI);
+
+  const handlePrintStaticColor = e => e.target.style.backgroundColor = "black";
+
+  const handleClickOnBoard = e => {
+    if (!clickPrint.active) {
+      clickPrint.active = true;
+      for (let i = 0; i < boardUI.children.length; i++) {
+        boardUI.children[i].addEventListener("pointerover", handlePrintStaticColor);
+      }
+    }
+    else {
+      clickPrint.active = false;
+      for (let i = 0; i < boardUI.children.length; i++) {
+        boardUI.children[i].removeEventListener("pointerover", handlePrintStaticColor);
+      }
+    }
+  }
+
+  boardUI.addEventListener("click", handleClickOnBoard);
+
+
+
+
+
+
+
+
+
+
 
 
 }
